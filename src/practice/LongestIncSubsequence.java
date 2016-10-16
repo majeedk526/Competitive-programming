a3878a3b7f101d4533d4684a56afcca56880aa14
Majeed <majeedk526@gmail.com>
2016-10-15 23:06:28 -0800

    feat: completed longest increasing subsequence

---
+++ src/practice/LongestIncSubsequence.java
@@ -0,0 +1,49 @@
+package practice;
+
+import java.util.Arrays;
+
+public class LongestIncSubsequence {
+
+	public static void main(String[] args) {
+		int[] arr = {3, 4, -1, 0, 6, 2, 3};
+		LongestIncSubsequence liss = new LongestIncSubsequence();
+		
+		int max = liss.lis(arr);
+
+		System.out.println(max);
+		
+	}
+	
+	public int lis(int[] arr){
+		
+		int i=1;
+		int j=0;
+		int max = 0;
+		
+		int seq[] = new int[arr.length];
+		Arrays.fill(seq, 1);
+		
+		while(i != arr.length){
+			
+			if(arr[j] < arr[i]){
+				
+				if(seq[i] < seq[j]+1){
+					seq[i] = seq[j] + 1;
+				}
+				
+				if(max< seq[i]){max = seq[i];}
+			}
+			
+			j++;
+			
+			if(j==i){
+				j=0;
+				i++;
+			}
+		}
+		
+		
+		return max;
+	}
+
+}
