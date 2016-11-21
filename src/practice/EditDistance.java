c5d371d133b247f2a20cb48b283d4baf9fca81f1
Majeed <majeedk526@gmail.com>
2016-11-21 01:19:33 -0800

    added dynamic programming solution for edit distance problem

--- src/practice/EditDistance.java
+++ src/practice/EditDistance.java
@@ -8,8 +8,10 @@ public class EditDistance {
 		String str1 = "INTENTION";
 		String str2 = "EXECUTION";
 		
-		System.out.println(ed.fed(str1, str2, str1.length()-1, str2.length()-1));
+		//System.out.println(ed.fed(str1, str2, str1.length()-1, str2.length()-1));
 
+		ed.fedDyn(str1, str2, str1.length(), str2.length());
+		
 	}
 	
 	public int fed(String str1, String str2, int m, int n){
@@ -24,6 +26,51 @@ public class EditDistance {
 		int min = Math.min(1 + fed(str1, str2, m-1,n), 1+fed(str1, str2, m, n-1));
 		return Math.min(min, 1+ fed(str1, str2,m-1,n-1));
 		
+	}
+	
+	public void fedDyn(String str1, String str2, int m, int n){
+		
+		int arr[][] = new int[m][n];
+		for(int i=0; i<arr.length; i++)
+			arr[i][0] = i;
+		
+		//printArray(arr);
+		
+		for(int i=0; i<arr[0].length; i++)
+			arr[0][i] = i;
+		
+		//printArray(arr);
+		
+		for(int i=1; i<m; i++){
+			for(int j=1; j<n; j++){
+				
+				if(str1.charAt(i-1)== str2.charAt(j-1)){
+					arr[i][j] = arr[i-1][j-1];
+				} else {
+					
+									//deletion, insertion
+					int min = Math.min(1+arr[i-1][j], 1+arr[i][j-1]);
+					arr[i][j] = Math.min(min, 1+arr[i-1][j-1]); //substitution
+					
+				}
+				
+			}
+		}
+		
+		printArray(arr);
+		System.out.println("min distance = " + arr[arr.length-1][arr[0].length-1]);
+		
+	}
+	
+	public void printArray(int[][] arr){
+		
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
 	}
 
 }
